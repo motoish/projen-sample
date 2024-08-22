d3de9014f2d08ebb458cc66d33d488094a048670
motoish <yuan.zhang@gmx.com>
2024-08-22 17:38:03 +0900

    fix: upgrade aws-cdk to v2.154.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,13 +2,13 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'motoish',
   authorAddress: 'yuan.zhang@gmx.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.154.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
   name: 'projen-sample',
   projenrcTs: true,
   repositoryUrl: 'git@github.com-motoish:motoish/projen-sample.git',
-
+  depsUpgrade: true,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
