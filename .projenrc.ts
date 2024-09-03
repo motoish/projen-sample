9be13fc77ac6c2a46dc90546cc7c7392b5ab1a25
motoish <yuan.zhang@gmx.com>
2024-09-03 15:51:50 +0900

    fix: set minNodeVersion to v18.8.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -18,6 +18,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
       singleQuote: true,
     },
   },
+  minNodeVersion: "18.8.0",
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
